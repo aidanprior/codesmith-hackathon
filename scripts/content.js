cdd165ac8b71d2ae8e2145312cb48d60dd558358
aidanprior <priorlyaidan@yahoo.com>
2024-08-28 20:10:45 -0400

    adds highlighting elements on doubleclick

--- scripts/content.js
+++ scripts/content.js
@@ -7,4 +7,18 @@
 console.log('running content.js');
 document
   .querySelectorAll('*')
-  .forEach((element) => (element.style.outline = '1px red solid'));
+  .forEach((element) => (element.style.outline = '1px red solid')); // add this to a button from the extension?
+
+let selectedElement;
+let backgroundStorage; // used to put the same background color back after the user deselects
+document.addEventListener('dblclick', (event) => {
+  const underMouse = document.elementFromPoint(event.clientX, event.clientY);
+  if (underMouse) {
+    if (selectedElement)
+      selectedElement.style.backgroundColor = backgroundStorage; // revert old selected element to its original background
+
+    selectedElement = underMouse; // remember this element to be reverted later
+    backgroundStorage = underMouse.style.backgroundColor; // remember the elements background to be reverted later
+    underMouse.style.backgroundColor = '#ec94fb';
+  }
+});
